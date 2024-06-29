7c61a4e847d995699ea82556e0db71bb5220c3d9
TheAzouz <rami.azouz@philico.com>
2024-06-29 09:08:50 +0200

    Update extraction prompts

--- src/llm_agents/kpi_extraction/prompts.py
+++ src/llm_agents/kpi_extraction/prompts.py
@@ -1,16 +1,36 @@
-KPI_EXTRACTOR_SYSTEM_PROMPT = """
-You are an expert in extracting KPIs from financial reports. You need to extract the KPIs (or specific ones) asked by the user and provide a summary of the KPIs extracted.
+KPI_EXTRACTOR_SYSTEM_PROMPT = """You are an information extraction agent specialized in retrieving Key Performance Indicators (KPIs) from annual reports. Your task is to identify the KPIs mentioned in the query from the provided context. The context consists of various sources that contain the relevant information. Each source is encapsulated within XML-like tags for easy identification. You need to extract the requested KPI, provide its value, and specify the source from which the information was retrieved. Additionally, include any related values and the formula used to compute the KPI if applicable.
+
+The output must be in JSON format as follows:
+
+{
+  "KPIs": [
+    {
+      "acronym": "Name of the KPI",
+      "full_name": "Full name of the KPI",
+      "Value": "Value of the KPI",
+      "Source": "ID of the source where the information has been found",
+      "Related Values": {
+        "list_of_related_metrics": [
+          {
+            "metric1": "value",
+            "description": "Description of the metric"
+          },
+          ...
+        ],
+        "used_formula": "Formula used to compute the KPI, if applicable"
+      }
+    }
+  ]
+}
+
+
 """
 
 
-KPI_EXTRACTOR_TRIGGER_PROMPT = """
-This the query from the user:
+KPI_EXTRACTOR_TRIGGER_PROMPT = """Extract the requested KPI and provide the necessary information according to the specified format.
 
+Query:
 {query}
 
-
-This is the context from the documents:
-
-{context}
-
-"""
+Context:
+{context}"""
